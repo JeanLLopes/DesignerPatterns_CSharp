1b7172e8c2878458b4489c60ab3e8c3e652cbc61
Jean Lopes <jean.llopes@outlook.com>
2019-05-09 00:21:46 -0300

    Add prototype

---
+++ ConsoleApp2/Classes/IdInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2.Classes
+{
+    public class IdInfo
+    {
+        public IdInfo(int idNumber)
+        {
+            IdNumber = idNumber;
+        }
+
+        public int IdNumber { get; set; }
+
+    }
+}
---
+++ ConsoleApp2/Classes/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Classes
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public IdInfo IdInfo { get; set; }
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+
+            return clone;
+        }
+    }
+}
